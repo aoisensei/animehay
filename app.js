5a0a5fb261d502058ddf2e8a21eb1ea37df7033a
Nguyễn Lê Trung Thành <94786450+aoisensei@users.noreply.github.com>
2022-07-15 21:43:29 +0700

    uppp

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider__img').slick({
+        slidesToShow: 5,
+        infinite: true,
+        dots: true,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000,
+        draggable: false,
+    });
+  });
